50c4584779f451d864fd360f25bc281dd6c5ac8b
Phelimon Sarpaning <32636728+PhelimonSarpaning@users.noreply.github.com>
2020-09-18 17:20:45 -0500

    init react native

---
+++ ios/Pods/Target Support Files/Pods-iSchedule-tvOS/Pods-iSchedule-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iSchedule_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_iSchedule_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-iSchedule/Pods-iSchedule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iSchedule : NSObject
+@end
+@implementation PodsDummy_Pods_iSchedule
+@end

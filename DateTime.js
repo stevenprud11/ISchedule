52cb53929a240ecb86080a6211a05b99899e520c
stevenprud11 <stevenprud11@gmail.com>
2020-10-12 15:11:22 -0500

    feat: add ability to edit date and time for calendar event

--- DateTime.js
+++ DateTime.js
@@ -6,40 +6,36 @@ import { StyleSheet, Text, TouchableOpacity } from "react-native";
 const DateTime = () => {
   const [date, setDate] = useState(new Date());
   const [time, setTime] = useState(new Date(date.getFullYear(), date.getMonth(), date.getDate(), '12', '00', '00', '00'));
-  const [mode, setMode] = useState('date');
-  const [value, setValue] = useState(date);
-  const [show, setShow] = useState(false);
+  const [showDate, setShowDate] = useState(false);
+  const [showTime, setShowTime] = useState(false);
 
-  const onChange = (event, selectedValue) => {
-    setShow(Platform.OS === 'ios');
-    if (mode == 'date') {
-      const currentDate = selectedValue || new Date();
-      setDate(currentDate);
-      setValue(date)
-      setMode('date');
-      setShow(Platform.OS !== 'ios'); // to show the picker again in time mode
-    } else {
+
+  const onDateChange = (event, selectedValue) =>{
+    //setShow(Platform.OS === 'ios');
+    const currentDate = selectedValue || new Date();
+    setDate(currentDate);
+    setTime(currentDate);
+    //setValue(date)
+    //setShow(Platform.OS !== 'ios'); // to show the picker again in time mode
+  }
+
+  const onTimeChange = (event, selectedValue) => {
       const selectedTime = selectedValue || new Date();
       setTime(selectedTime);
-      setValue(time)
+      setDate(selectedTime);
+      //setValue(time)
       setDateTime(time);
-      setShow(Platform.OS === 'ios');
-      setMode('time');
-    }
-  };
+      //setShow(Platform.OS === 'ios');
+  }
 
-  const showMode = currentMode => {
-    setShow(true);
-    setMode(currentMode);
-   };
+  const showDateBool = currentShow => {
+    setShowDate(true);
+  }
 
-  const showDatepicker = () => {
-    showMode('date');
-  };
-
-  const showTimepicker = () => {
-    showMode('time');
-  };
+  const showTimeBool = currentShow => {
+    setShowTime(true);
+  }
+  
 
   const formatDate = (date, time) => {
     return `${date.getMonth()}/${date.getDay() +
@@ -52,38 +48,57 @@ const DateTime = () => {
 
   return (
     <View>
-        <Text>Current date: {new Date().toString()}</Text>
+        {/* <Text>Current date: {new Date().toString()}</Text>
         <Text>Date date: {date.getMonth()+1}/{date.getDate()}/{date.getFullYear()}</Text>
         <Text>Date time: {date.toLocaleTimeString()}</Text>
         <Text>Time date: {time.getMonth()+1}/{time.getDate()}/{time.getFullYear()}</Text>
-        <Text>Time time: {time.toLocaleTimeString()}</Text>
+        <Text>Time time: {time.toLocaleTimeString()}</Text> */}
 
         <Text>Event Date: {date.getMonth()+1}/{date.getDate()}/{date.getFullYear()} :: {date.toLocaleTimeString()}</Text>
-
-      <View>
-        <TouchableOpacity title="Show date picker" onPress={showDatepicker}>
+        <Text>Event Date: {date.toString()}</Text>
+      {/* <View>
+        <TouchableOpacity title="Show date picker" onPress={showDateBool}>
           <Text>{date.getDate}</Text>
         </TouchableOpacity>
-        <TouchableOpacity title="Show time picker" onPress={showTimepicker}>
+        <TouchableOpacity title="Show time picker" onPress={showTimeBool}>
           <Text>{time.getTime}</Text>
         </TouchableOpacity>
+      </View> */}
+
+      <View>
+          <Button onPress={showDateBool} title="Show date picker!" />
       </View>
       <View>
-          <Button onPress={showDatepicker} title="Show date picker!" />
-      </View>
-      <View>
-        <Button onPress={showTimepicker} title="Show time picker!" />
+        <Button onPress={showTimeBool} title="Show time picker!" />
       </View>
       
-      {show && (
+      {showDate && (
         <DateTimePicker
-          testID="dateTimePicker"r
+        testID="datePicker"
+        value={date}
+        mode={'date'}
+        display="default"
+        onChange={onDateChange}
+      />)}
+
+      {showTime && (
+        <DateTimePicker
+          testID="timePicker"
+          value={time}
+          mode={'time'}
+          display="default"
+          onChange={onTimeChange}
+      />)}
+
+      {/* {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
           value={date} // figure out how to show date vs time variable dependent on mode
           mode={mode}
           display="default"
           onChange={onChange}
         />
-      )}
+      )} */}
     </View>
   );
 };

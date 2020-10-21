74ed6e3c0ed4801476a09a2e517bd55818c7f7f1
hayes947 <50536550+hayes947@users.noreply.github.com>
2020-10-21 15:45:07 -0500

    Update IScheduleGetDateAndTime.js
    Added functionality that I forgot to get a second date and time from the string if one is available.

--- IScheduleGetDateAndTime.js
+++ IScheduleGetDateAndTime.js
@@ -1,5 +1,5 @@
 //NOTE: HOUR=24 indicates midnight. HOUR=0 indicates it was not able to be found.
-
+//I did not originally think to get two dates and times so a lot of the code for the second one is just quick and dirty to add that functionality
 
 var imgText="Hello, world! It is October 7, 2020 today at 12 PM.";
 var TF=imgText.split(" ");
@@ -9,6 +9,11 @@ var DAY=0;
 var YEAR=0;
 var HOUR=0;
 var MINUTE=0;
+var MONTH2=0;
+var DAY2=0;
+var YEAR2=0;
+var HOUR2=0;
+var MINUTE2=0;
 //let's work dates first
 function convertMonthToNumber(month){
     month=month.toLowerCase();
@@ -87,6 +92,21 @@ function MMDDYYYY(rawData){
     return 0;
 }
 
+function MMDDYYYY2(rawData){
+    var noSlash=rawData.split("/");
+    if (noSlash[0]>0 && noSlash[0]<13){
+        MONTH2=parseInt(noSlash[0],10);
+    }else{
+        DAY2=parseInt(noSlash[0],10);
+    }if(MONTH2==0){
+        MONTH2=parseInt(noSlash[1],10);
+    }else{
+        DAY2=parseInt(noSlash[1],10);
+    }
+    YEAR2=parseInt(noSlash[2],10);
+    return 0;
+}
+
 function colonTimes(rawData, index){
     var noColon=rawData.split(":");
     HOUR=parseInt(noColon[0],10);
@@ -116,6 +136,35 @@ function colonTimes(rawData, index){
     return 0;
 }
 
+function colonTimes2(rawData, index){
+    var noColon=rawData.split(":");
+    HOUR2=parseInt(noColon[0],10);
+    MINUTE2=parseInt(noColon[1],10);
+    console.log("HOUR parseInt nocolon = "+HOUR2);
+    if(HOUR<13){
+        var possibleAMPM=TF[index+1];
+        possibleAMPM=possibleAMPM.toLowerCase();
+        var AMPMSplit=possibleAMPM.split(".");
+        if(HOUR2!=12){
+            //console.log("We just entered HOUR!=12 with HOUR="+HOUR);
+            if(AMPMSplit[0]=="p" || AMPMSplit[0]=="pm"){
+                HOUR2+=12;
+            }
+        }
+        if(HOUR2==12){
+            //console.log("Detected HOUR2==12");
+            if(AMPMSplit[0]=="a" || AMPMSplit[0]=="am"){
+                HOUR2=24;
+                //console.log("Got HOUR2="+HOUR2);
+            }else{
+                HOUR2=12;
+                //console.log("Got HOUR2="+HOUR2);
+            }
+        }
+    }
+    return 0;
+}
+
 function milTimeConvert(milTime){
     HOUR+=parseInt(milTime.charAt(0),10);
     HOUR*=10;
@@ -124,6 +173,16 @@ function milTimeConvert(milTime){
     MINUTE+=parseInt(milTime.charAt(2),10);
     MINUTE*=10;
     MINUTE+=parseInt(milTime.charAt(3),10);
+}
+
+function milTimeConvert2(milTime){
+    HOUR2+=parseInt(milTime.charAt(0),10);
+    HOUR2*=10;
+    HOUR2+=parseInt(milTime.charAt(1),10);
+
+    MINUTE2+=parseInt(milTime.charAt(2),10);
+    MINUTE2*=10;
+    MINUTE2+=parseInt(milTime.charAt(3),10);
 }
 
 function hourAMPM(posTime, index){
@@ -148,6 +207,31 @@ function hourAMPM(posTime, index){
 
     if(HOUR>24 || HOUR<0){//there must be a mistake in this case
         HOUR=0;
+    }
+}
+
+function hourAMPM2(posTime, index){
+    HOUR2=posTime;
+    var possibleAMPM=TF[index+1];
+    possibleAMPM=possibleAMPM.toLowerCase();
+    var AMPMSplit=possibleAMPM.split(".");
+    //console.log(AMPMSplit);
+    if(HOUR2!=12){
+        if(AMPMSplit[0]=="p" || AMPMSplit[0]=="pm"){
+            HOUR2+=12;
+        }else{ 
+            if(AMPMSplit[0]!="a" || AMPMSplit[0]!="am"){//if it wasn't pm and it isnt am, then we made a mistake this isn't our format that we're after
+                HOUR2=0;
+            }
+        }
+    }if(HOUR2==12){
+        if(AMPMSplit[0]=="a" || AMPMSplit[0]=="am"){
+            HOUR2+=12;
+       }
+    }
+
+    if(HOUR2>24 || HOUR2<0){//there must be a mistake in this case
+        HOUR2=0;
     }
 }
 
@@ -184,6 +268,34 @@ function findDateAndTime(){
                 MMDDYYYY(TF[i]);
                 i++;
             }
+        }else if(MONTH2==0){
+            MONTH2=convertMonthToNumber(TF[i]);
+            if(MONTH2!=0){
+                i++;//we can safely do an i++ here because if we just found the month there's no way the word we're on currently can be anything else that we need
+                if(DAY2==0){
+                    DAY2=checkForValidDay(TF[i]); //our day is probably right after the month we just found, most likely.
+                    if(DAY2!=0){
+                        i++;
+                        YEAR2=checkForValidYear(TF[i]);
+                        if(YEAR2!=0){
+                            i++;
+                        }
+                    }else{//We look now for day month year
+                        DAY2=checkForValidDay(TF[i-2]);
+                        if(DAY2!=0){
+                            YEAR2=checkForValidYear(TF[i]);
+                        }
+                        if(YEAR2!=0){
+                            i++;
+                        }
+                    }
+                }
+            }
+            //Get MM/DD/YYYY and DD/MM/YYYY
+            if(TF[i].charAt(1)=="/" || TF[i].charAt(2)=="/"){
+                MMDDYYYY2(TF[i]);
+                i++;
+            }
         }
         //looking for times
         if(HOUR==0){
@@ -208,6 +320,28 @@ function findDateAndTime(){
                     hourAMPM(possibleTime, i);
                 }
             }
+        }else if(HOUR2==0){
+            //Try to get HH:MM times (colontimes)
+            if(TF[i].charAt(1)==":" || TF[i].charAt(2)==":"){
+                colonTimes2(TF[i], i);
+                //console.log("Now exited colonTimes");
+            }
+            //trying to get HHMM times (military time format)
+            else if(TF[i].length==5){
+                var possibleTime=parseInt(TF[i],10);
+                if(possibleTime>0 && possibleTime<=2400){
+                    milTimeConvert2(TF[i]);
+                }
+            }
+            //check for HOUR AM/PM
+            else if(TF[i].length==1 || TF[i].length==2){
+                //console.log("suspected HOUR AM/PM");
+                var possibleTime=parseInt(TF[i],10);
+                //console.log("possibleTime="+possibleTime);
+                if(possibleTime>0 && possibleTime<=12){
+                    hourAMPM2(possibleTime, i);
+                }
+            }
         }
         i++;
     }
@@ -216,6 +350,11 @@ function findDateAndTime(){
     console.log("YEAR: "+YEAR);
     console.log("HOUR: "+HOUR);
     console.log("MINUTE: "+MINUTE);
+    console.log("MONTH2: "+MONTH2);
+    console.log("DAY2: "+DAY2);
+    console.log("YEAR2: "+YEAR2);
+    console.log("HOUR2: "+HOUR2);
+    console.log("MINUTE2: "+MINUTE2);
 }
 
 

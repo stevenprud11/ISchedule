084428572f4e8ddd6221dbdd06c12f2fe41cbf10
stevenprud11 <stevenprud11@gmail.com>
2020-10-26 15:16:10 -0500

    config: add dependencies for nav

--- android/app/src/main/java/com/ischedule/MainActivity.java
+++ android/app/src/main/java/com/ischedule/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ischedule;
-
+import com.facebook.react.ReactActivityDelegate;  
+import com.facebook.react.ReactRootView;  
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +14,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "iSchedule";
   }
+
+  @Override  
+protected ReactActivityDelegate createReactActivityDelegate() {  
+    return new ReactActivityDelegate(this, getMainComponentName()) {  
+        @Override  
+        protected ReactRootView createRootView() {  
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);  
+        }  
+    };  
+}  
 }

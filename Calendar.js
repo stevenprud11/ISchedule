d8b192041ee85b449125c9dc14b975e61e56d59b
stevenprud11 <stevenprud11@gmail.com>
2020-10-26 15:24:04 -0500

    feat: add calendar and camera page

---
+++ Calendar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+export default class Calendar extends Component {
+    constructor(props){
+        state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <View>
+                <Text>Calendar Page</Text>
+            </View>
+        )
+    }
+}
---
+++ Camera.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    TextInput,
+    TouchableOpacity,
+  } from 'react-native';
+
+
+export default class Camera extends Component {
+    constructor(props){
+        super(props)
+        state = {
+
+        }
+    }
+
+    render(){
+
+        return(
+            <View>
+                <Text>Camera Page</Text>
+            </View>
+        );
+    }
+
+
+
+}

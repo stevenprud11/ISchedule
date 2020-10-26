67ba4572cecba5ae8220e799af1b5ef0f53c909a
Richie Joshuva <46910831+rjoshuva@users.noreply.github.com>
2020-10-26 15:21:49 -0500

    removed unused modules
    RNTextDetector, TesseractOCR

--- ios/Pods/Headers/Private/RNTextDetector/RNTextDetector.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-text-detector/ios/RNTextDetector.h
--- ios/Pods/Target Support Files/RNTextDetector/RNTextDetector-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RNTextDetector : NSObject
-@end
-@implementation PodsDummy_RNTextDetector
-@end

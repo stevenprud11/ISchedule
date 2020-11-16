f833f1ea3da20806863906f8878dbd18e66c0658
stevenprud11 <stevenprud11@gmail.com>
2020-11-16 15:51:57 -0600

    feat: add camera functionailty

--- Camera.js
+++ Camera.js
@@ -6,32 +6,91 @@ import {
     Button,
     TextInput,
     TouchableOpacity,
+    SafeAreaView,
+    Alert,
   } from 'react-native';
+  import {PureComponent} from 'react';
+import {RNCamera} from 'react-native-camera';
+
+//import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+import RNFS from 'react-native-fs';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
-export default class Camera extends Component {
+export default class Camera extends PureComponent {
     constructor(props){
         super(props)
-        state = {
-
+        this.state = {
+          takingPic: false,
         }
     }
 
+    takePicture = async () => {
+      if (this.camera && !this.state.takingPic) {
+         
+        let options = {
+          quality: 0.85,
+          base64: true,
+          fixOrientation: true,
+          forceUpOrientation: true,
+        };
+
+        this.setState({takingPic: true});
+
+        try {
+          const data = await this.camera.takePictureAsync(options);
+          RNFS.readFile(data.uri, 'base64')
+            .then(res =>{
+              console.log(res);
+              this.props.navigation.push("OCR");
+            });
+            //console.log(data.uri);
+        
+        } catch (err) {
+          Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
+          return;
+        } finally {
+          this.setState({takingPic: false});
+        }
+
+      }
+    };
+
     render(){
         const {navigation} = this.props;
-        return(
-          <View style={{alignItems: "center", justifyContent: "center"}}>
-          
-            <Text>Camera Screen</Text>
-            
-            <Button 
-                title="Take Picture"
-                onPress={() => navigation.push('OCR')}
-            />
-            
-          </View>
+        return (
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            captureAudio={false}
+            style={{flex: 1}}
+            type={RNCamera.Constants.Type.back}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'We need your permission to use your camera',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}>
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    style={styles.btnAlignment}
+                    >
+                    <Button title="camera" onPress={this.takePicture} size={100} color="#fff" />
+                </TouchableOpacity>
+            </RNCamera>
         );
     }
 }
 
+const styles = StyleSheet.create({
+  btnAlignment: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      marginBottom: 20,
+  },
+});
 
